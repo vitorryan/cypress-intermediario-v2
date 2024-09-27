fd2fe6e659e4c578f4c623d3a4501230c4aba453
Vitor Ryan da Rosa <vitorryanrosaaa@hotmail.com>
2024-09-26 21:50:19 -0300

    configurando o setup do projeto de testes

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,9 @@
+const { fa } = require("@faker-js/faker");
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: 'http://localhost',
   },
+  video: false,
 });
